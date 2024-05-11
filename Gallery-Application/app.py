2675f26b1c676c8360ab79b812d940bc41974a2c
Projit Chandra <projitchandra32@gmail.com>
2024-05-11 21:02:49 +0530

    Updated Service template

--- Gallery-Application/app.py
+++ Gallery-Application/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/app/')
 def hello_world():
    return 'Hello Worlds'
+
+@app.route('/healthCheck/', methods=['GET'])
+def health_check():
+   return jsonify({"Status":"UP"})
 
 if __name__ == '__main__':
    app.run(port=5000, host="0.0.0.0")
