60c69e480959637602770dde88da8b9e24bad45a
Avirup Chattaraj <avirupchattaraj1234@duck.com>
2024-06-08 15:47:31 +0530

    Individual services for the backend

---
+++ Backend-Application/multimedia-download/multimedia-download/src/main/java/com/prolabs/multimedia_download/MultimediaDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.prolabs.multimedia_download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimediaDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultimediaDownloadApplication.class, args);
+	}
+
+}
