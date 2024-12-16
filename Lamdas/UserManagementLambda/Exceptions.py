afa77171d985a98fa30eff0b704364966e6f91b7
Projit Chandra <projitchandra32@gmail.com>
2024-12-16 18:54:06 +0530

    cognito lambda router config settlement

--- Lamdas/UserManagementLambda/Exceptions.py
+++
@@ -1,2 +0,0 @@
-class DuplicateRouteException(ValueError):
-    pass
--- Lamdas/UserManagementLambda/UserManagementService.py
+++ Lamdas/UserManagementLambda/UserManagementService.py
@@ -1,18 +1,20 @@
-import traceback
+import os
+import uuid
 
 import boto3
-import botocore.exceptions
 import hmac
 import hashlib
 import base64
-import json
-from APIRegistry import ApiRouteRegistry
 
-USER_POOL_ID = ''
-CLIENT_ID = ''
-CLIENT_SECRET = ''
+from botocore.exceptions import ClientError
+
+from APIRegistry import APIRouteRegistry, APIRequestException, APIServerException
+
+USER_POOL_ID = os.getenv("USER_POOL_ID")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 client = boto3.client('cognito-idp')
-router = ApiRouteRegistry()
+router = APIRouteRegistry()
 
 
 def get_secret_hash(username):
@@ -30,9 +32,7 @@ def sign_up(event, request):
     username = event['username']
     email = event["email"]
     password = event['password']
-    
-    try:
-        resp = client.sign_up(
+    return client.sign_up(
             ClientId=CLIENT_ID,
             SecretHash=get_secret_hash(username),
             Username=username,
@@ -45,194 +45,62 @@ def sign_up(event, request):
             ])
 
 
-    except client.exceptions.UsernameExistsException as e:
-        return {"error": False,
-                "success": True,
-                "message": "This username already exists",
-                "data": None}
-    except client.exceptions.InvalidPasswordException as e:
-
-        return {"error": False,
-                "success": True,
-                "message": "Password should have Caps,\
-                          Special chars, Numbers",
-                "data": None}
-    except client.exceptions.UserLambdaValidationException as e:
-        return {"error": False,
-                "success": True,
-                "message": "Email already exists",
-                "data": None}
-
-    except Exception as e:
-        return {"error": False,
-                "success": True,
-                "message": str(e),
-                "data": None}
-
-    return {"error": False,
-            "success": True,
-            "message": "Please confirm your signup, \
-                        check Email for validation code",
-            "data": None}
-
 @router.register_route(methods=["POST"], path="/users/confirm-sign-up")
 def confirm_signup(event, request):
-    
-    try:
-        username = event['username']
-        code = event['code']
-        response = client.confirm_sign_up(
-            ClientId=CLIENT_ID,
-            SecretHash=get_secret_hash(username),
-            Username=username,
-            ConfirmationCode=code,
-            ForceAliasCreation=False,
-        )
-    except client.exceptions.UserNotFoundException:
-        # return {"error": True, "success": False, "message": "Username doesnt exists"}
-        return event
-    except client.exceptions.CodeMismatchException:
-        return {"error": True, "success": False, "message": "Invalid Verification code"}
-
-    except client.exceptions.NotAuthorizedException:
-        return {"error": True, "success": False, "message": "User is already confirmed"}
-
-    except Exception as e:
-        return {"error": True, "success": False, "message": f"Unknown error {e.__str__()} "}
-
-    return event
+    username = event['username']
+    code = event['code']
+    return client.confirm_sign_up(
+        ClientId=CLIENT_ID,
+        SecretHash=get_secret_hash(username),
+        Username=username,
+        ConfirmationCode=code,
+        ForceAliasCreation=False,
+    )
 
 @router.register_route(methods=["POST"], path="/users/resend-verification-code")
 def resend_verification(event, request):
-    
-    try:
-        username = event['username']
-        response = client.resend_confirmation_code(
-            ClientId=CLIENT_ID,
-            SecretHash=get_secret_hash(username),
-            Username=username,
-        )
-    except client.exceptions.UserNotFoundException:
-        return {"error": True, "success": False, "message": "Username doesnt exists"}
-
-    except client.exceptions.InvalidParameterException:
-        return {"error": True, "success": False, "message": "User is already confirmed"}
-
-    except Exception as e:
-        return {"error": True, "success": False, "message": f"Unknown error {e.__str__()} "}
-
-    return {"error": False, "success": True}
+    username = event['username']
+    return client.resend_confirmation_code(
+        ClientId=CLIENT_ID,
+        SecretHash=get_secret_hash(username),
+        Username=username,
+    )
 
 @router.register_route(methods=["POST"], path="/users/forgot-password")
 def forgot_password(event, request):
-    
-    try:
-        username = event['username']
-        response = client.forgot_password(
-            ClientId=CLIENT_ID,
-            SecretHash=get_secret_hash(username),
-            Username=username,
+    username = event['username']
+    return client.forgot_password(
+        ClientId=CLIENT_ID,
+        SecretHash=get_secret_hash(username),
+        Username=username,
 
-        )
-    except client.exceptions.UserNotFoundException:
-        return {"error": True,
-                "data": None,
-                "success": False,
-                "message": "Username doesnt exists"}
-
-    except client.exceptions.InvalidParameterException:
-        return {"error": True,
-                "success": False,
-                "data": None,
-                "message": f"User <{username}> is not confirmed yet"}
-
-    except client.exceptions.CodeMismatchException:
-        return {"error": True,
-                "success": False,
-                "data": None,
-                "message": "Invalid Verification code"}
-
-    except client.exceptions.NotAuthorizedException:
-        return {"error": True,
-                "success": False,
-                "data": None,
-                "message": "User is already confirmed"}
-
-    except Exception as e:
-        return {"error": True,
-                "success": False,
-                "data": None,
-                "message": f"Uknown    error {e.__str__()} "}
-
-    return {
-        "error": False,
-        "success": True,
-        "message": f"Please check your Registered email id for validation code",
-        "data": None}
+    )
 
 @router.register_route(methods=["POST"], path="/users/confirm-forgot-password")
 def confirm_forgot_password(event, request):
-    
-    try:
-        username = event['username']
-        password = event['password']
-        code = event['code']
-        client.confirm_forgot_password(
-            ClientId=CLIENT_ID,
-            SecretHash=get_secret_hash(username),
-            Username=username,
-            ConfirmationCode=code,
-            Password=password,
-        )
-    except client.exceptions.UserNotFoundException as e:
-        return {"error": True,
-                "success": False,
-                "data": None,
-                "message": "Username doesnt exists"}
-
-    except client.exceptions.CodeMismatchException as e:
-        return {"error": True,
-            "success": False,
-            "data": None,
-            "message": "Invalid Verification code"}
-
-    except client.exceptions.NotAuthorizedException as e:
-        return {"error": True,
-            "success": False,
-            "data": None,
-            "message": "User is already confirmed"}
-
-    except Exception as e:
-        return {"error": True,
-            "success": False,
-            "data": None,
-            "message": f"Unknown error {e.__str__()} "}
-
-    return {"error": False,
-            "success": True,
-            "message": f"Password has been changed successfully",
-            "data": None}
+    username = event['username']
+    password = event['password']
+    code = event['code']
+    return client.confirm_forgot_password(
+        ClientId=CLIENT_ID,
+        SecretHash=get_secret_hash(username),
+        Username=username,
+        ConfirmationCode=code,
+        Password=password,
+    )
 
 @router.register_route(methods=["POST"], path="/users/initiate-auth")
 def initiate_auth(client, username, password):
     secret_hash = get_secret_hash(username)
-    try:
-      resp = client.admin_initiate_auth(
-                 UserPoolId=USER_POOL_ID,
-                 ClientId=CLIENT_ID,
-                 AuthFlow='ADMIN_USER_PASSWORD_AUTH',
-                 AuthParameters={
-                     'USERNAME': username,
-                    'SECRET_HASH': secret_hash,
-                     'PASSWORD': password,
-                  })
-    except client.exceptions.NotAuthorizedException:
-        return None, "The username or password is incorrect"
-    except client.exceptions.UserNotConfirmedException:
-        return None, "User is not confirmed"
-    except Exception as e:
-        return None, e.__str__()
-    return resp, None
+    return client.admin_initiate_auth(
+        UserPoolId=USER_POOL_ID,
+        ClientId=CLIENT_ID,
+        AuthFlow='ADMIN_USER_PASSWORD_AUTH',
+        AuthParameters={
+            'USERNAME': username,
+            'SECRET_HASH': secret_hash,
+            'PASSWORD': password,
+        })
 
 @router.register_route(methods=["POST"], path="/users/login")
 def login(event, request):
@@ -243,20 +111,8 @@ def login(event, request):
                     "success": False,
                     "message": f"{field} is required",
                     "data": None}
-    resp, msg = initiate_auth(client, event["username"], event["password"])
-    if msg != None:
-        return {'message': msg,
-                "error": True, "success": False, "data": None}
-    if resp.get("AuthenticationResult"):
-        return {'message': "success",
-                "error": False,
-                "success": True,
-                "data": resp
-                }
-    else:  # this code block is relevant only when MFA is enabled
-        return {"error": True,
-                "success": False,
-                "data": None, "message": None}
+    return initiate_auth(client, event["username"], event["password"])
+
 
 
 @router.register_route(methods=["POST"], path="/users/forgot")
@@ -273,27 +129,18 @@ def logout(event, request):
 @router.register_route(methods=["POST"], path="/users/refresh-token")
 def refresh_login(event, request):
     secret_hash = get_secret_hash(event["username"])
-    try:
-        resp = client.admin_initiate_auth(
-            UserPoolId=USER_POOL_ID,
-            ClientId=CLIENT_ID,
-            AuthFlow='REFRESH_TOKEN_AUTH',
-            AuthParameters={
-                'REFRESH_TOKEN': event["RefreshToken"],
-                'SECRET_HASH': secret_hash,
-            })
-    except client.exceptions.NotAuthorizedException as e:
-        print(e)
-        return None, "The username or password is incorrect"
-    except client.exceptions.UserNotConfirmedException:
-        return None, "User is not confirmed"
-    except Exception as e:
-        return None, e.__str__()
-    return resp, None
+    return client.admin_initiate_auth(
+        UserPoolId=USER_POOL_ID,
+        ClientId=CLIENT_ID,
+        AuthFlow='REFRESH_TOKEN_AUTH',
+        AuthParameters={
+            'REFRESH_TOKEN': event["RefreshToken"],
+            'SECRET_HASH': secret_hash,
+        })
 
-if __name__ == "__main__":
-    print(router.routes)
-    #print(sign_up({"username":"", "email":"@gmail.com", "password":""}))
+# if __name__ == "__main__":
+    # print(router.routes)
+    # print(sign_up({"username":"", "email":"@gmail.com", "password":""}))
     # print(confirm_signup({"username":"", "code": ""}))
     # print(login({"username":"@gmail.com", "password":""}))
     # print(get_user({"AccessToken":""}))
@@ -302,4 +149,11 @@ if __name__ == "__main__":
 
 
 def handle_request(event, context):
-    return router.serve(event, context)
+    try:
+        return router.serve(event, context)
+    except ClientError as err:
+        raise APIRequestException(err.response["Error"]["Code"],
+            err.response["Error"]["Message"])
+
+    except Exception as e:
+        raise APIServerException(uuid.uuid4().__str__(), e)
